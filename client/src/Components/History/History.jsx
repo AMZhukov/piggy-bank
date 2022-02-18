7745bba1777a1c4345e2fa356f0cf83fe533a658
Жуков Андрей Михайлович <zhukov@softlab.ru>
2022-02-18 22:11:01 +0500

    feat/added package `@zachleat/details-utils` to frontend. For add animate tag `details`

--- client/src/Components/History/History.jsx
+++ client/src/Components/History/History.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
 import { HistoryItem } from './HistoryItem';
+import '@zachleat/details-utils';
 
 export const History = ({ transactions, deleteTransaction }) => {
   return (
     <section className="history">
-      <details>
-        <summary className="history__summary">
-          <h3 className="history__title">История расходов</h3>
-        </summary>
-        {transactions.length > 0 ? (
-          <ul className="history__list">
-            {transactions.map((transaction) => {
-              return (
-                <HistoryItem
-                  transaction={transaction}
-                  key={transaction._id}
-                  deleteTransaction={deleteTransaction}
-                />
-              );
-            })}
-          </ul>
-        ) : (
-          <div className="history__item history__item_no-transactions">
-            Транзакций в данном месяце нет
-          </div>
-        )}
-      </details>
+      <details-utils animate>
+        <details>
+          <summary className="history__summary">
+            <h3 className="history__title">История расходов</h3>
+          </summary>
+          {transactions.length > 0 ? (
+            <ul className="history__list">
+              {transactions.map((transaction) => {
+                return (
+                  <HistoryItem
+                    transaction={transaction}
+                    key={transaction._id}
+                    deleteTransaction={deleteTransaction}
+                  />
+                );
+              })}
+            </ul>
+          ) : (
+            <div className="history__item history__item_no-transactions">
+              Транзакций в данном месяце нет
+            </div>
+          )}
+        </details>
+      </details-utils>
     </section>
   );
 };
